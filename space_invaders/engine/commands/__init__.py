42427d23d2f8cdc49998f190e51ade5639f94eba
Andrey Mozzhevilov <71andy@mail.ru>
2023-02-13 20:09:06 +0300

    fix linter error

--- space_invaders/engine/commands/__init__.py
+++ space_invaders/engine/commands/__init__.py
@@ -1,6 +1,5 @@
-from space_invaders.engine.commands.rotate import Rotate
 from space_invaders.engine.commands.burn_fuel import BurnFuel
-
+from space_invaders.engine.commands.rotate import Rotate
 
 __all__ = (
     'BurnFuel',
--- space_invaders/tests/test_rotate.py
+++ space_invaders/tests/test_rotate.py
@@ -1,10 +1,10 @@
 import pytest
+
 from space_invaders.engine.commands import Rotate
 from space_invaders.engine.interfaces.rotate import Rotable
 
 
 class MockObjRotate(Rotable):
-
     def __init__(self, direction: int, directions_number: int, angular_velocity: int):
         self._direction = direction
         self._directions_number = directions_number
@@ -28,17 +28,17 @@ class MockObjRotate(Rotable):
 
 
 @pytest.mark.parametrize(
-    "direction, directions_number, angular_velocity, result_direction",
+    ('direction', 'directions_number', 'angular_velocity', 'result_direction'),
     [
         (2, 7, 1, 3),
-        (3, 5, 3, 6)
-    ]
+        (3, 5, 3, 6),
+    ],
 )
 def test_success(direction, directions_number, angular_velocity, result_direction):
     rotable_obj = MockObjRotate(
         direction=direction,
         directions_number=directions_number,
-        angular_velocity=angular_velocity
+        angular_velocity=angular_velocity,
     )
     Rotate(rotable_obj).execute()
     assert rotable_obj.direction == result_direction
